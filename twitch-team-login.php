39e3e06829b4dc41df13b069ced59e361e9818f9
Anne Dorko <anne@dorko.tv>
2021-06-27 10:11:32 +0200

    Plugin init

---
+++ twitch-team-login.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Twitch Team Login
+Plugin URI:  https://github.com/annedorko/twitch-team-login
+Description: Allow members of a Twitch team to login to a WordPress website.
+Version:     0.0.1
+Author:      Anne Dorko
+Author URI:  https://dorko.dev/
